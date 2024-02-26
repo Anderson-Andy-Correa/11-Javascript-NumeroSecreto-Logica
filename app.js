3677517e60976ec8c16233fa16a09fa97ac7e2fe
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-26 19:44:33 -0300

    Concatenar alerta de chute certo

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Boas vindo ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 8;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
 // Comentário
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o numero secreto (5)')
+    console.log(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
 } else {
     alert('Você errou :(')
 }
