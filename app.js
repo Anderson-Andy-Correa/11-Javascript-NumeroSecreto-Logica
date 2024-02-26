f09efbaa9ee9887b1fea2bb246214bed5d4eebca
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-26 19:40:42 -0300

    inserir alerta de valor de chute incorreto

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindo ao jogo do número secreto!');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// Comentário
 if (chute == numeroSecreto) {
     console.log('Isso ai! Você descobriu o numero secreto (5)')
+} else {
+    alert('Você errou :(')
 }
