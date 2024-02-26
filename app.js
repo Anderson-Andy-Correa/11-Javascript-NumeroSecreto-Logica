cd6357731a90c37d6541c7c90fbf5d3fa8b20070
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-26 20:11:23 -0300

    Inserir contador de tentativas

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 alert('Boas vindo ao jogo do número secreto!');
 let numeroSecreto = 2;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 let chute;
+let tentativas = 0;
 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
+    tentativas ++;
 
     // Comentário
     if (chute == numeroSecreto) {
-        console.log(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
+        console.log(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`)
     } else {
         if (chute < numeroSecreto) {
-            alert(`O numero secreto é maior que ${chute}`)
+            alert(`O numero secreto é maior que ${chute}`);
         } else {
-            alert(`O numero secreto é menor que ${chute}`)
-        }
-    }
-}
+            alert(`O numero secreto é menor que ${chute}`);
+        };
+    };
+};
