1ed0cffd2c3664a2c7b202641338ea576ed26933
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-26 19:55:31 -0300

    Inserir loop de verificação do chute

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 alert('Boas vindo ao jogo do número secreto!');
 let numeroSecreto = 2;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 
-// Comentário
-if (chute == numeroSecreto) {
-    console.log(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
-} else {
-    if (chute < numeroSecreto) {
-        alert(`O numero secreto é maior que ${chute}`)
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // Comentário
+    if (chute == numeroSecreto) {
+        console.log(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
     } else {
-        alert(`O numero secreto é menor que ${chute}`)
+        if (chute < numeroSecreto) {
+            alert(`O numero secreto é maior que ${chute}`)
+        } else {
+            alert(`O numero secreto é menor que ${chute}`)
+        }
     }
 }
