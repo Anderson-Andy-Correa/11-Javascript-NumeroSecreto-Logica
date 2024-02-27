6e7da9b896e3b6fa857b58ed8fd6b8db68c0c8e6
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-02-26 21:21:25 -0300

    Inserir gerador de numero aleatório

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Boas vindo ao jogo do número secreto!');
-let numeroSecreto = 2;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 0;
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     tentativas ++;
 
     // Comentário
@@ -20,8 +20,5 @@ while (chute != numeroSecreto) {
     };
 };
 
-if (tentativas > 1) {
-    alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`)
-} else {
-    alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativa.`)
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`)
